6b943989eac52abd42556e98c1a26f898f9d0659
jdjin3000 <57910677+jdjin3000@users.noreply.github.com>
2020-05-19 23:04:50 +0900

    Update Programmers_124 나라의 숫자_L2.py

--- 기타/Programmers_124 나라의 숫자_L2.py
+++ 기타/Programmers_124 나라의 숫자_L2.py
@@ -2,27 +2,27 @@ def solution(N):
     third = ""
     
     while N >= 3:
-        third += str(N%3)
-        N = N//3
+        q, r = divmod(N, 3)
+        third += str(r)
+        N = q
     
     third += str(N)
 
     third = list(map(int, list(third[::-1])))
-    print(third)
 
     while 0 in third:
         for i in range(len(third) - 1):
             if third[i+1] == 0:
                 third[i] -= 1
                 third[i+1] = 3
-    
-    print(third)
-    # third = str(int("".join(map(str, third))))
-    # third = third.replace('2', '4')
-    # third = third.replace('1', '2')
-    # third = third.replace('0', '1')
+        
+        for i in range(len(third)):
+            if third[0] != 0:
+                break
+            del third[0]
 
-    # answer = third
-    # return answer
+    third = str(int("".join(map(str, third))))
+    third = third.replace('3', '4')
 
-solution(21)
+    answer = third
+    return answer
